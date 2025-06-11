10071351bd2f6b066349efc80ca8da1cb7a86ecb
Jamerson Carlos Dos Santos <64469941+JamersonCarlos@users.noreply.github.com>
2025-06-11 00:43:07 -0300

    Update funcionario.dart

--- lib/domain/entities/funcionario.dart
+++ lib/domain/entities/funcionario.dart
@@ -1,31 +1,41 @@
 class Funcionario {
+  final String nome;
   final String email;
   final String codigo_cartao;
   final int curso_id;
   final int cargo_id;
+  String? _cargoNome;
 
   Funcionario({
+    this.nome = '',
     required this.email,
     required this.codigo_cartao,
     required this.curso_id,
     required this.cargo_id,
-  });
+    String? cargoNome,
+  }) : _cargoNome = cargoNome;
+  
+  String? get cargo => _cargoNome;
 
   Map<String, dynamic> toJson() {
     return {
+      'nome': nome,
       'email': email,
       'codigo_cartao': codigo_cartao,
       'curso_id': curso_id,
       'cargo_id': cargo_id,
+      'cargo_nome': _cargoNome,
     };
   }
 
   factory Funcionario.fromJson(Map<String, dynamic> json) {
     return Funcionario(
+      nome: json['nome'] ?? '',
       email: json['email'],
       codigo_cartao: json['codigo_cartao'],
       curso_id: json['curso_id'],
       cargo_id: json['cargo_id'],
+      cargoNome: json['cargo_nome'],
     );
   }
 }
