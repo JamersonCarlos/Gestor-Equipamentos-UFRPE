690aa6d609e86c2a2653297dfe5afad89ab0d1aa
Jamerson Carlos Dos Santos <64469941+JamersonCarlos@users.noreply.github.com>
2025-06-15 17:37:36 -0300

    adicionando filtro por código de tombamento

--- lib/presentation/widgets/projetores_list.dart
+++ lib/presentation/widgets/projetores_list.dart
@@ -1,10 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:gestor_uso_projetores_ufrpe/utils/inputDecoration.dart';
 import '../../domain/entities/projetor.dart';
 import '../../core/theme/app_colors.dart';
 import '../../services/projetorService.dart';
 
 class ProjetoresList extends StatefulWidget {
-  final Function? onListUpdated; // Callback opcional para notificar atualizações
+  final Function?
+      onListUpdated; // Callback opcional para notificar atualizações
 
   const ProjetoresList({Key? key, this.onListUpdated}) : super(key: key);
 
@@ -15,23 +17,16 @@ class ProjetoresList extends StatefulWidget {
 class _ProjetoresListState extends State<ProjetoresList> {
   final _projetorService = ProjetorService();
 
-  @override
-  void initState() {
-    super.initState();
-  }
-
   Future<void> _deleteProjetor(Projetor projetor) async {
     try {
       await _projetorService.deleteProjetor(projetor.codigo_tombamento);
       ScaffoldMessenger.of(context).showSnackBar(
-        const SnackBar( 
+        const SnackBar(
           content: Text('Projetor removido com sucesso!'),
           backgroundColor: AppColors.primary,
         ),
       );
-      setState(() {
-        
-      });
+      setState(() {});
     } catch (e) {
       ScaffoldMessenger.of(context).showSnackBar(
         SnackBar(
@@ -42,149 +37,238 @@ class _ProjetoresListState extends State<ProjetoresList> {
     }
   }
 
+  Future<List<Projetor>>? _futureProjetores;
+  List<Projetor> _todosProjetores = [];
+  List<Projetor> _projetoresFiltrados = [];
+
+  TextEditingController _codigoTombController = TextEditingController();
+
+  Future<List<Projetor>> fetchProjetores() async {
+    try {
+      _todosProjetores = await _projetorService.getProjetores();
+      _projetoresFiltrados = _todosProjetores;
+      return _todosProjetores;
+    } catch (e) {
+      ScaffoldMessenger.of(context).showSnackBar(
+        SnackBar(
+          content: Text('Erro ao buscar projetores: $e'),
+          backgroundColor: AppColors.error,
+        ),
+      );
+      return [];
+    }
+  }
+
+  void _filterProjetores() {
+    if (_codigoTombController.text.isEmpty) {
+      setState(() {
+        _projetoresFiltrados = _todosProjetores;
+      });
+    } else {
+      setState(() {
+        _projetoresFiltrados = _todosProjetores
+            .where((projetor) => projetor.codigo_tombamento
+                .toLowerCase()
+                .contains(_codigoTombController.text.toLowerCase()))
+            .toList();
+      });
+    }
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    _futureProjetores = fetchProjetores();
+  }
+
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder(
-      future: _projetorService.getProjetores(),
-      builder: (context, asyncSnapshot) {
-        if (asyncSnapshot.connectionState == ConnectionState.waiting) {
-          return const Center(child: CircularProgressIndicator());
-        } else if (asyncSnapshot.hasError) {
-          return const Center(child: Text('Erro: Sem conexão com o servidor')); 
-        } else if (!asyncSnapshot.hasData || asyncSnapshot.data!.isEmpty) {
-          return const Center(child: Text('Nenhum projetor cadastrado.'));
-        }
-        
-        List<Projetor> _projetores = asyncSnapshot.data!;
-        return ListView.builder(
-              itemCount: _projetores.length,
-              itemBuilder: (context, index) {
-                final projetor = _projetores[index];
-                return Card(
-                  elevation: 4, 
-                  child: Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: Row(
-                      crossAxisAlignment: CrossAxisAlignment.center,
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.center,
+    return Column(
+      children: [
+        Container(
+          constraints: const BoxConstraints(maxWidth: 1600),
+          child: Row(
+            children: [
+              SizedBox(
+                width: 400,
+                child: TextFormField(
+                  controller: _codigoTombController,
+                  decoration: buildInputDecoration(
+                    'Código de Tombamento',
+                    'Digite o código de tombamento do projetor',
+                    const Icon(Icons.search,
+                        color: AppColors.primary, size: 24),
+                  ),
+                  onChanged: (value) => _filterProjetores(),
+                ),
+              ),
+            ],
+          ),
+        ),
+        Expanded(
+          child: FutureBuilder(
+              future: _futureProjetores,
+              builder: (context, asyncSnapshot) {
+                if (asyncSnapshot.connectionState == ConnectionState.waiting) {
+                  return const Center(child: CircularProgressIndicator());
+                } else if (asyncSnapshot.hasError) {
+                  return const Center(
+                      child: Text('Erro: Sem conexão com o servidor'));
+                } else if (!asyncSnapshot.hasData ||
+                    asyncSnapshot.data!.isEmpty) {
+                  return const Center(
+                      child: Text('Nenhum projetor cadastrado.'));
+                }
+
+                return ListView.builder(
+                  itemCount: _projetoresFiltrados.length,
+                  itemBuilder: (context, index) {
+                    final projetor = _projetoresFiltrados[index];
+                    return Card(
+                      elevation: 4,
+                      child: Padding(
+                        padding: const EdgeInsets.all(16.0),
+                        child: Row(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
-                            Text(
-                              projetor.codigo_tombamento,
-                              style: const TextStyle(
-                                fontSize: 18,
-                                fontWeight: FontWeight.bold,
-                              ),
+                            Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              mainAxisAlignment: MainAxisAlignment.center,
+                              children: [
+                                Text(
+                                  projetor.codigo_tombamento,
+                                  style: const TextStyle(
+                                    fontSize: 18,
+                                    fontWeight: FontWeight.bold,
+                                  ),
+                                ),
+                                const SizedBox(height: 8),
+                                Text(
+                                  'Modelo: ${projetor.modelo}',
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  'Marca: ${projetor.marca}',
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                Text(
+                                  'Cor: ${projetor.cor}',
+                                  style: TextStyle(
+                                    color: Colors.grey[600],
+                                    fontSize: 14,
+                                  ),
+                                ),
+                              ],
                             ),
-                            const SizedBox(height: 8),
-                            Text(
-                              'Modelo: ${projetor.modelo}',
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 14,
-                              ),
-                            ),
-                            Text(
-                              'Marca: ${projetor.marca}',
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 14,
-                              ),
-                            ),
-                            Text(
-                              'Cor: ${projetor.cor}',
-                              style: TextStyle(
-                                color: Colors.grey[600],
-                                fontSize: 14,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          spacing: 10,
-                          children: [
-                            ElevatedButton.icon(
-                              onPressed: () {
-                                showDialog(
-                                  context: context,
-                                  builder: (context) { 
-                                    TextEditingController codigoTagController = TextEditingController(text: projetor.codigo_tag);
-                                    return AlertDialog(
-                                      title: const Center(child: Text('Código da Tag', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14),)),
-                                      content: TextFormField(
-                                        controller: codigoTagController,
-                                        decoration: InputDecoration(
-                                          suffixIcon: Row(
-                                            mainAxisSize: MainAxisSize.min,
-                                            children: [
-                                              IconButton(
-                                                icon: const Icon(Icons.copy),
-                                                onPressed: () {
-                                                  // Implementar a cópia do código
-                                                  ScaffoldMessenger.of(context).showSnackBar(
-                                                    const SnackBar(
-                                                      content: Text('Código copiado!'),
-                                                      backgroundColor: AppColors.primary,
-                                                    ),
-                                                  );
-                                                },
+                            Row(
+                              spacing: 10,
+                              children: [
+                                ElevatedButton.icon(
+                                  onPressed: () {
+                                    showDialog(
+                                      context: context,
+                                      builder: (context) {
+                                        TextEditingController
+                                            codigoTagController =
+                                            TextEditingController(
+                                                text: projetor.codigo_tag);
+                                        return AlertDialog(
+                                          title: const Center(
+                                              child: Text(
+                                            'Código da Tag',
+                                            style: TextStyle(
+                                                fontWeight: FontWeight.bold,
+                                                fontSize: 14),
+                                          )),
+                                          content: TextFormField(
+                                            controller: codigoTagController,
+                                            decoration: InputDecoration(
+                                              suffixIcon: Row(
+                                                mainAxisSize: MainAxisSize.min,
+                                                children: [
+                                                  IconButton(
+                                                    icon:
+                                                        const Icon(Icons.copy),
+                                                    onPressed: () {
+                                                      // Implementar a cópia do código
+                                                      ScaffoldMessenger.of(
+                                                              context)
+                                                          .showSnackBar(
+                                                        const SnackBar(
+                                                          content: Text(
+                                                              'Código copiado!'),
+                                                          backgroundColor:
+                                                              AppColors.primary,
+                                                        ),
+                                                      );
+                                                    },
+                                                  ),
+                                                ],
                                               ),
-                                            ],
+                                            ),
                                           ),
-                                        ),
-                                      ),
-                                      actions: [
-                                        TextButton(
-                                          onPressed: () {
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text('Fechar'),
-                                        ),
-                                        TextButton(
-                                          onPressed: () {
-                                            ScaffoldMessenger.of(context).showSnackBar(
-                                              const SnackBar(
-                                                content: Text('Código copiado!'),
-                                                backgroundColor: AppColors.primary,
-                                              ),
-                                            );
-                                            Navigator.of(context).pop();
-                                          },
-                                          child: const Text('Salvar'),
-                                        ),
-                                      ],
+                                          actions: [
+                                            TextButton(
+                                              onPressed: () {
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text('Fechar'),
+                                            ),
+                                            TextButton(
+                                              onPressed: () {
+                                                ScaffoldMessenger.of(context)
+                                                    .showSnackBar(
+                                                  const SnackBar(
+                                                    content:
+                                                        Text('Código copiado!'),
+                                                    backgroundColor:
+                                                        AppColors.primary,
+                                                  ),
+                                                );
+                                                Navigator.of(context).pop();
+                                              },
+                                              child: const Text('Salvar'),
+                                            ),
+                                          ],
+                                        );
+                                      },
                                     );
                                   },
-                                );
-                              },
-                              icon: const Icon(Icons.qr_code),
-                              label: const Text('Código Tag'),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.primary,
-                                foregroundColor: Colors.white,
-                              ),
-                            ),
-                            ElevatedButton.icon(
-                              icon: const Icon(Icons.delete),
-                              label: const Text('Remover'),
-                              onPressed: () => _deleteProjetor(projetor),
-                              style: ElevatedButton.styleFrom(
-                                backgroundColor: AppColors.error,
-                                foregroundColor: Colors.white,
-                              ),
+                                  icon: const Icon(Icons.qr_code),
+                                  label: const Text('Código Tag'),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: AppColors.primary,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                ),
+                                ElevatedButton.icon(
+                                  icon: const Icon(Icons.delete),
+                                  label: const Text('Remover'),
+                                  onPressed: () => _deleteProjetor(projetor),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: AppColors.error,
+                                    foregroundColor: Colors.white,
+                                  ),
+                                ),
+                              ],
                             ),
                           ],
                         ),
-                      ],
-                    ),
-                  ),
+                      ),
+                    );
+                  },
                 );
-              },
-            );
-      }
+              }),
+        ),
+      ],
     );
   }
 }
+
