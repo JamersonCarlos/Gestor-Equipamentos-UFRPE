236f1e1204e9ef0de349036b760f62bb7b0afe24
Jamerson Carlos Dos Santos <64469941+JamersonCarlos@users.noreply.github.com>
2025-07-23 23:57:56 -0300

    bug resolve

--- lib/presentation/widgets/projetores_list.dart
+++ lib/presentation/widgets/projetores_list.dart
@@ -1,5 +1,3 @@
-import 'dart:nativewrappers/_internal/vm/lib/ffi_allocation_patch.dart';
-
 import 'package:flutter/material.dart';
 import 'package:gestor_uso_projetores_ufrpe/domain/entities/defeito.dart';
 import 'package:gestor_uso_projetores_ufrpe/services/defeito_service.dart';
